502b8f3215f2cb744c2b66b10c9506623e075d21
IumoInfinium <44368355+IumoInfinium@users.noreply.github.com>
2022-01-14 13:37:47 +0530

    added option for video/playlist

--- script.py
+++ script.py
@@ -7,14 +7,27 @@ try:
 except:
     print("\nError while installing PyTube")
 
-playlist_to_download=input("\nPlaylist to download : ")
-
-playlist = pytube.Playlist(playlist_to_download)
-
-cur_dir=input("\nLocation to save the videos : ")
-
-for video in playlist.videos:
-    print('\nDownloading : {} \n\tURL : {}'.format(video.title, video.watch_url))
-    # video.streams.filter(type='video', progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(cur_dir)
-    yt = pytube.YouTube(video.watch_url)
+print("Download type :")
+print("1. Video")
+print("2. Playlist")
+ch=input("Select from above choices:")
+if ch=="1" or ch=="Video":
+    video_url=input("\nVideo url to download : ")
+    yt = pytube.YouTube(video_url)
+    cur_dir=input("\nLocation to save the video : ")
     yt.streams.get_highest_resolution().download(cur_dir)
+
+elif ch=="2" or ch=="Playlist":
+    playlist_to_download=input("\nPlaylist to download : ")
+
+    playlist = pytube.Playlist(playlist_to_download)
+
+    cur_dir=input("\nLocation to save the videos : ")
+
+    for video in playlist.videos:
+        print('\nDownloading : {} \nURL : {}'.format(video.title, video.watch_url))
+        # video.streams.filter(type='video', progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(cur_dir)
+        yt = pytube.YouTube(video.watch_url)
+        yt.streams.get_highest_resolution().download(cur_dir)
+else:
+    print("Invalid input check again!")
